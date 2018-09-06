febfc59535a897ca56fd1e73971b40470db4aceb
김성박 <urstory@gmail.com>
2018-09-06 11:43:52 +0900

    first commit

---
+++ first/src/examples/first/CalcTest.java
@@ -0,0 +1,18 @@
+package examples.first;
+
+public class CalcTest {
+    public static void main(String[] args){
+        Calc cal1 = new Calc(500);
+
+        System.out.println(cal1.getValue());
+
+        cal1.plus(50);
+
+        System.out.println(cal1.getValue());
+
+        cal1.plus(100);
+
+        System.out.println(cal1.getValue());
+
+    }
+}
---
+++ first/src/examples/first/NHLottoMachine.java
@@ -0,0 +1,28 @@
+package examples.first;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class NHLottoMachine implements LottoMachine{
+    private List<LottoBall> balls;
+
+    @Override
+    public void put(List<LottoBall> balls) {
+        this.balls = balls;
+    }
+
+    @Override
+    public void mix() {
+        Collections.shuffle(balls);
+    }
+
+    @Override
+    public List<LottoBall> getBalls() {
+        List<LottoBall> result = new ArrayList<>();
+        for(int i = 0; i < 6; i++){
+            result.add(balls.get(i));
+        }
+        return result;
+    }
+}
