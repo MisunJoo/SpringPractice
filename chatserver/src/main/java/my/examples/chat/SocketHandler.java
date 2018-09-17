2c41bf5342de56e4ba2aed922b62bc6843690a45
김성박 <urstory@gmail.com>
2018-09-17 11:54:57 +0900

    ChatHandler 작성

--- chatserver/src/main/java/my/examples/chat/SocketHandler.java
+++ chatserver/src/main/java/my/examples/chat/SocketHandler.java
@@ -1,5 +1,6 @@
 package my.examples.chat;
 
+import java.io.*;
 import java.net.Socket;
 
 public class SocketHandler extends Thread{
@@ -13,6 +14,24 @@ public class SocketHandler extends Thread{
 
     @Override
     public void run() {
-        System.out.println("socket으로 열심히 일하자.");
+        System.out.println("SocketHandler Start");
+        BufferedReader in = null;
+        PrintWriter out = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+            String line = null;
+            while((line = in.readLine()) != null){ // 네트워 연결이 끊어질때지 null이 나오면 끝.
+                out.println(line);
+                out.flush();
+            }
+        }catch(Exception ex){
+            System.out.println("Chat Handler close");
+        }finally {
+            System.out.println("Chat Handler finally");
+            try{ in.close(); }catch(Exception e){}
+            try{ out.close(); }catch(Exception e){}
+        }
     }
 }
+
