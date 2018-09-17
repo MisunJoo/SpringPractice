6b3f08220a8e8090cdd42b2270c2c64460c628f3
김성박 <urstory@gmail.com>
2018-09-17 12:29:35 +0900

    추가메소드

--- chatserver/src/main/java/my/examples/chat/ChatServer.java
+++ chatserver/src/main/java/my/examples/chat/ChatServer.java
@@ -1,14 +1,36 @@
 package my.examples.chat;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 public class ChatServer extends Thread{
     private int port; // 서버가 실행되는 port
+    private Set<PrintWriter> socketSet = null;
 
     public ChatServer(int port){
         this.port = port;
+        this.socketSet = new CopyOnWriteArraySet();
+    }
+    
+    public void addPrintWriter(PrintWriter out){
+        socketSet.add(out);
+    }
+
+    public void removePrintWriter(PrintWriter out){
+        socketSet.remove(out);
+    }
+
+    public void broadcast(String msg){
+        for(PrintWriter out : socketSet){
+            try {
+                out.println(msg);
+                out.flush();
+            }catch(Exception ex){}
+        }
     }
 
     @Override
