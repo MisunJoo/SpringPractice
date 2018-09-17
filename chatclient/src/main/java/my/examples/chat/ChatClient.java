f359503a6829a2d232a74b1e7b1a9e6608572177
김성박 <urstory@gmail.com>
2018-09-17 13:29:01 +0900

    클라이언트작성

--- chatclient/src/main/java/my/examples/chat/ChatClient.java
+++ chatclient/src/main/java/my/examples/chat/ChatClient.java
@@ -1,5 +1,9 @@
 package my.examples.chat;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ChatClient extends Thread{
@@ -15,13 +19,35 @@ public class ChatClient extends Thread{
     @Override
     public void run() {
         Socket socket = null;
+        BufferedReader in = null;
+        PrintWriter out = null;
+        BufferedReader keyboard = null;
         try{
             socket = new Socket(ip, port);
+            keyboard = new BufferedReader(new InputStreamReader(System.in));
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            System.out.println("채팅할 이름을 입력하세요.");
+            this.name = keyboard.readLine();
+            out.println(name);
+            out.flush();
+            InputHandler handler = new InputHandler(in);
+            handler.start();
+
+            String line = null;
+            while((line = keyboard.readLine()) != null){
+                if(line.equals("/quit")) break;
+                out.println(line);
+                out.flush();
+            }
 
         }catch(Exception ex){
             ex.printStackTrace();
         }finally {
             try{ socket.close(); }catch(Exception e){}
+            try{ in.close(); }catch(Exception e){}
+            try{ out.close(); }catch(Exception e){}
         }
     } // run
 }
--- chatserver/src/main/java/my/examples/chat/Main.java
+++ chatserver/src/main/java/my/examples/chat/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args){
         ChatServer chatServer = new ChatServer(11000);
         chatServer.start();
+        System.out.println("서버가 실행!");
     }
 }
 // http://localhost:11000
