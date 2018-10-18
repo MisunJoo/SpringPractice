b8106d5b903ce18ab6db5e65aa03ed6d67f1a60d
urstory <urstory@gmail.com>
2018-10-18 11:52:34 +0900

    에제 실행

--- DI/scopedbeans/src/main/java/examples/scopedbeans/Notepad.java
+++ DI/scopedbeans/src/main/java/examples/scopedbeans/Notepad.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-@Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+//@Component
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class Notepad {
+
+    public Notepad(){
+        System.out.println("Notepad()");
+    }
   // the details of this class are inconsequential to this example
 }
--- DI/scopedbeans/src/main/java/examples/scopedbeans/UniqueThing.java
+++ DI/scopedbeans/src/main/java/examples/scopedbeans/UniqueThing.java
@@ -2,7 +2,10 @@ package examples.scopedbeans;
 
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class UniqueThing {
+    public UniqueThing(){
+        System.out.println("UniqueThing");
+    }
   // the details of this class are inconsequential to this example
 }
